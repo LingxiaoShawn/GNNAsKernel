db5e6805b2d487aef025e2fb7b10d66e0a5c23d6
LingxiaoShawn <562998710@qq.com>
2021-09-29 16:40:52 -0400

    update scripts

---
+++ scripts/molhiv_regularized.sh
@@ -0,0 +1,7 @@
+python -m train.molhiv model.dropout 0.5 model.num_layers 2 model.mini_layers 0  num_workers 12 train.runs 5 device 0 &
+python -m train.molhiv model.dropout 0.5 model.num_layers 6 model.mini_layers 0  num_workers 12 train.runs 5 device 1 &
+python -m train.molhiv model.dropout 0.5 model.num_layers 2 model.embs "(2,)"    num_workers 12 train.runs 5 device 2 &
+python -m train.molhiv model.dropout 0.5 model.num_layers 2 model.embs "(2,0)"   num_workers 12 train.runs 5 device 3 &
+python -m train.molhiv model.dropout 0.5 model.num_layers 6 model.embs "(2,)"    num_workers 12 train.runs 5 device 4 &
+python -m train.molhiv model.dropout 0.5 model.num_layers 6 model.embs "(2,0)"   num_workers 12 train.runs 5 device 5 &
+
---
+++ scripts/tuning_gak_hp/hp_via_gin-ak_counting.sh
@@ -0,0 +1,4 @@
+python -m train.counting model.embs "(0,1,2)" 
+python -m train.counting model.embs "(0,1,)" 
+python -m train.counting model.embs "(0,2,)" 
+python -m train.counting model.embs "(1,2,)" 
