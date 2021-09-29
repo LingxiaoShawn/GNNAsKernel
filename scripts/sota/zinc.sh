99361f8fced8226d05509ed1b87c0440c9fe6878
LingxiaoShawn <562998710@qq.com>
2021-09-29 17:34:15 -0400

    add scripts

---
+++ scripts/sota/zinc.sh
@@ -0,0 +1,25 @@
+# GIN
+# python -m train.zinc model.mini_layers 0
+# GIN-AK
+# python -m train.zinc 
+
+# GCN
+python -m train.zinc model.mini_layers 0 model.gnn_type GCNConv
+# GCN-AK
+python -m train.zinc model.gnn_type GCNConv
+# SimplifiedPNA
+python -m train.zinc model.mini_layers 0 model.gnn_type SimplifiedPNAConv
+# SimplifiedPNA-AK
+python -m train.zinc model.gnn_type SimplifiedPNAConv
+
+# GAT
+python -m train.zinc model.mini_layers 0 model.gnn_type GATConv
+# GAT-AK
+python -m train.zinc model.mini_layers 1 model.gnn_type GATConv
+# GatedGCN
+python -m train.zinc model.mini_layers 0 model.gnn_type ResGatedGraphConv 
+# GatedGCM-AK
+python -m train.zinc model.mini_layers 1 model.gnn_type ResGatedGraphConv 
+
+
+# 
