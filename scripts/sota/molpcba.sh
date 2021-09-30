a08f28fc9fd8ee509c9f5418274e7e1df9914afb
LingxiaoShawn <562998710@qq.com>
2021-09-30 02:04:35 -0400

    update

--- scripts/sota/molpcba.sh
+++ scripts/sota/molpcba.sh
@@ -1,15 +1,16 @@
 # GIN
 python -m train.molpcba model.mini_layers 0
-# GIN-AK
-python -m train.molpcba model.mini_layers 1
 # GCN
 python -m train.molpcba model.mini_layers 0 model.gnn_type GCNConv
-# GCN-AK
-python -m train.molpcba model.mini_layers 1 model.gnn_type GCNConv
 # SimplifiedPNA
 python -m train.molpcba model.mini_layers 0 model.gnn_type SimplifiedPNAConv
+# GIN-AK
+python -m train.molpcba model.mini_layers 1
+# GCN-AK
+python -m train.molpcba model.mini_layers 1 model.gnn_type GCNConv
 # SimplifiedPNA-AK
 python -m train.molpcba model.mini_layers 1 model.gnn_type SimplifiedPNAConv
+
 
 
 # GIN
