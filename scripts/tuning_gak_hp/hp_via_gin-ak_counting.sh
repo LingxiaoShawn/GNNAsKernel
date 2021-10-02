1a09d85b66b765351759ab5697389b9dc0941773
LingxiaoShawn <562998710@qq.com>
2021-10-01 23:17:27 -0400

    change positions

--- scripts/tuning_gak_hp/hp_via_gin-ak_counting.sh
+++
@@ -1,4 +0,0 @@
-python -m train.counting model.embs "(0,1,2)" 
-python -m train.counting model.embs "(0,1,)" 
-python -m train.counting model.embs "(0,2,)" 
-python -m train.counting model.embs "(1,2,)" 
