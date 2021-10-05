028f4cab36ce27ce5ccdea2bdb389e2aabad687c
LingxiaoShawn <562998710@qq.com>
2021-10-04 22:25:31 -0400

    update scripts

--- scripts/left_steps/3-sampling-curve-only-zinc-gin.sh
+++ scripts/left_steps/3-sampling-curve-only-zinc-gin.sh
@@ -1,8 +1,14 @@
-redundancys=(1 2 3 4 5)
-# 1. zinc, GCN-AK GIN-AK PNA-AK
-for r in "${redundancys[@]}"; do
-    python -m train.zinc --config train/configs/molhiv_sampling.yaml sampling.redundancy $r
-done
+# redundancys=(1 2 3 4 5)
+# # 1. zinc, GCN-AK GIN-AK PNA-AK
+# for r in "${redundancys[@]}"; do
+#     python -m train.zinc --config train/configs/molhiv_sampling.yaml sampling.redundancy $r num_workers 14 device 5
+# done
 
 # later can add cifar 10
+
+python -m train.cifar10 --config train/configs/cifar10_sampling.yaml num_workers 12  sampling.redundancy 4 sampling.random_rate 0.28  device 0 &
+python -m train.cifar10 --config train/configs/cifar10_sampling.yaml num_workers 12  sampling.redundancy 5 sampling.random_rate 0.35  device 1 &
+
+#   redundancy: 3
+#   random_rate: 0.2 
 
--- scripts/left_steps/4-embedding-types-zinc-cifar-gin.sh
+++ scripts/left_steps/4-embedding-types-zinc-cifar-gin.sh
@@ -1,9 +1,14 @@
-# zinc
-python -m train.zinc model.embs "(0,1)" 
-python -m train.zinc model.embs "(0,2)" 
-python -m train.zinc model.embs "(1,2)" 
+# # zinc
+# python -m train.zinc model.embs "(0,1)"  device 4
+# python -m train.zinc model.embs "(0,2)"  device 4
+# python -m train.zinc model.embs "(1,2)"  device 4
 
-# cifar10
-python -m train.cifar10 model.embs "(0,1)" 
-python -m train.cifar10 model.embs "(0,2)" 
-python -m train.cifar10 model.embs "(1,2)" 
+# # cifar10
+# python -m train.cifar10 model.embs "(0,1)"  device 4
+
+
+
+python -m train.cifar10 subgraph.hops  1 num_workers 12 device 2 &
+python -m train.cifar10 model.hops_dim 0 num_workers 12 device 3 &
+python -m train.cifar10 model.embs "(0,2)"  num_workers 12 device 4 &
+python -m train.cifar10 model.embs "(1,2)"  num_workers 12 device 5 &
