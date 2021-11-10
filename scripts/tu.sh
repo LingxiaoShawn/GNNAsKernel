ec54eb5fa004c1eb511f106c94e74e0b4c5bdcd5
LingxiaoShawn <562998710@qq.com>
2021-11-10 15:10:23 -0500

    update tudataset

---
+++ scripts/tu.sh
@@ -0,0 +1,8 @@
+
+
+python -m train.tu_datasets dataset PTC_MR  model.mini_layers 1  device 0 &
+python -m train.tu_datasets dataset PROTEINS  model.mini_layers 1  device 1 &
+python -m train.tu_datasets dataset NCI1  model.mini_layers 1  device 2 &
+python -m train.tu_datasets dataset IMDB-BINARY  model.mini_layers 1  device 3 &
+python -m train.tu_datasets dataset IMDB-MULTI  model.mini_layers 1  device 4 &
+python -m train.tu_datasets dataset REDDIT-BINARY  model.mini_layers 1  device 5 &
