878ce313c6bab7643c29914eaa1ba84a5b6e796c
LingxiaoShawn <562998710@qq.com>
2021-11-11 23:19:18 -0500

    update tu and additional ablation

--- scripts/left_steps/4-embedding-types-zinc-cifar-gin.sh
+++ scripts/left_steps/4-embedding-types-zinc-cifar-gin.sh
@@ -3,12 +3,21 @@
 # python -m train.zinc model.embs "(0,2)"  device 4
 # python -m train.zinc model.embs "(1,2)"  device 4
 
+
 # # cifar10
 # python -m train.cifar10 model.embs "(0,1)"  device 4
+# python -m train.cifar10 model.embs "(0,2)"  num_workers 12 device 4 &
+# python -m train.cifar10 model.embs "(1,2)"  num_workers 12 device 5 &
+# python -m train.cifar10 subgraph.hops  1 num_workers 12 device 2 &
+# python -m train.cifar10 model.hops_dim 0 num_workers 12 device 3 &
 
+## additional ablation study
+python -m train.cifar10 model.embs "(0,)"  device 2 &
+python -m train.cifar10 model.embs "(1,)"  device 3 &
+python -m train.cifar10 model.embs "(2,)"  device 4 &
+python -m train.cifar10 model.embs "(1,)" model.hops_dim 0 device 5 &
 
-
-python -m train.cifar10 subgraph.hops  1 num_workers 12 device 2 &
-python -m train.cifar10 model.hops_dim 0 num_workers 12 device 3 &
-python -m train.cifar10 model.embs "(0,2)"  num_workers 12 device 4 &
-python -m train.cifar10 model.embs "(1,2)"  num_workers 12 device 5 &
+python -m train.zinc model.embs "(0,)"  device 1
+python -m train.zinc model.embs "(1,)"  device 1
+python -m train.zinc model.embs "(2,)"  device 1
+python -m train.zinc model.embs "(1,)" model.hops_dim 0 device 1
--- scripts/tu.sh
+++ scripts/tu.sh
@@ -1,12 +1,8 @@
-
-
 python -m train.tu_datasets dataset PTC_MR  model.mini_layers 1  device 0 &
 python -m train.tu_datasets dataset PROTEINS  model.mini_layers 1  device 1 &
 python -m train.tu_datasets dataset NCI1  model.mini_layers 1  device 2 &
 python -m train.tu_datasets dataset IMDB-BINARY  model.mini_layers 1  device 3 &
 python -m train.tu_datasets dataset IMDB-MULTI  model.mini_layers 1  device 4 &
-python -m train.tu_datasets dataset REDDIT-BINARY  model.mini_layers 1  device 5 &
+python -m train.tu_datasets dataset REDDIT-BINARY  model.mini_layers 1 subgraph.walk_length 8 subgraph.walk_repeat 4 device 5 &
 
 
-
-python -m train.tu_datasets --config train/configs/tu_sampling.yaml dataset REDDIT-BINARY  subgraph.hops 2 model.mini_layers 1 
