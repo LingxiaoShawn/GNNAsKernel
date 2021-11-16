acb58b5b169f10fe73b40b96731804143e97eb93
LingxiaoShawn <562998710@qq.com>
2021-11-16 11:38:03 -0500

    add additional ablation study

--- scripts/left_steps/4-embedding-types-zinc-cifar-gin.sh
+++ scripts/left_steps/4-embedding-types-zinc-cifar-gin.sh
@@ -12,12 +12,17 @@
 # python -m train.cifar10 model.hops_dim 0 num_workers 12 device 3 &
 
 ## additional ablation study
-python -m train.cifar10 model.embs "(0,)"  device 2 &
-python -m train.cifar10 model.embs "(1,)"  device 3 &
-python -m train.cifar10 model.embs "(2,)"  device 4 &
-python -m train.cifar10 model.embs "(1,)" model.hops_dim 0 device 5 &
+# python -m train.cifar10 model.embs "(0,)"  device 2 &
+# python -m train.cifar10 model.embs "(1,)"  device 3 &
+# python -m train.cifar10 model.embs "(2,)"  device 4 &
+# python -m train.cifar10 model.embs "(1,)" model.hops_dim 0 device 5 &
 
-python -m train.zinc model.embs "(0,)"  device 1
-python -m train.zinc model.embs "(1,)"  device 1
-python -m train.zinc model.embs "(2,)"  device 1
-python -m train.zinc model.embs "(1,)" model.hops_dim 0 device 1
+# python -m train.zinc model.embs "(0,)"  device 1
+# python -m train.zinc model.embs "(1,)"  device 1
+# python -m train.zinc model.embs "(2,)"  device 1
+# python -m train.zinc model.embs "(1,)" model.hops_dim 0 device 1
+
+
+
+python -m train.cifar10 model.embs "(0,1)" model.hops_dim 0 device 5 &
+python -m train.zinc model.embs "(0,1)" model.hops_dim 0 device 1 &
--- scripts/left_steps/7-each-task-simulation.sh
+++ scripts/left_steps/7-each-task-simulation.sh
@@ -1,11 +1,11 @@
 
 # counting substructure
-tasks=(0 1 2 3)
-for task in "${tasks[@]}"; do
-    # python -m train.counting task $task num_workers 8 model.mini_layers 0 model.gnn_type GCNConv         
-    # python -m train.counting task $task num_workers 8 model.mini_layers 0 model.gnn_type GINEConv        
-    python -m train.counting task $task num_workers 8 model.num_layers 4 model.mini_layers 0 model.gnn_type SimplifiedPNAConv 
-done
+# tasks=(0 1 2 3)
+# for task in "${tasks[@]}"; do
+#     # python -m train.counting task $task num_workers 8 model.mini_layers 0 model.gnn_type GCNConv         
+#     # python -m train.counting task $task num_workers 8 model.mini_layers 0 model.gnn_type GINEConv        
+#     python -m train.counting task $task num_workers 8 model.num_layers 4 model.mini_layers 0 model.gnn_type SimplifiedPNAConv 
+# done
 
 # # regress graph property
 # tasks=(0 1 2)
@@ -14,3 +14,22 @@ done
 #     # python -m train.graph_property task $task num_workers 8 model.mini_layers 0 model.gnn_type GINEConv       
 #     python -m train.graph_property task $task num_workers 8 model.mini_layers 0 model.gnn_type SimplifiedPNAConv  
 # done
+
+# additional ablation study
+
+tasks=(0 1 2 3)
+for task in "${tasks[@]}"; do
+    # python -m train.counting task $task num_workers 8 model.mini_layers 1 model.embs "(0, 1)"  model.gnn_type GINEConv
+    # python -m train.counting task $task num_workers 8 model.mini_layers 1 model.embs "(1,)"  model.gnn_type GINEConv 
+    python -m train.counting task $task num_workers 8 model.mini_layers 1 model.embs "(0, 1)" model.hops_dim 0 model.gnn_type GINEConv 
+    python -m train.counting task $task num_workers 8 model.mini_layers 1 model.hops_dim 0 model.gnn_type GINEConv         
+done
+
+tasks=(0 1 2)
+for task in "${tasks[@]}"; do
+    # python -m train.graph_property task $task num_workers 8 model.mini_layers 1 model.embs "(0, 1)" model.gnn_type GINEConv 
+    # python -m train.graph_property task $task num_workers 8 model.mini_layers 1 model.embs "(1,)" model.gnn_type GINEConv
+    python -m train.graph_property task $task num_workers 8 model.mini_layers 1 model.embs "(0, 1)" model.hops_dim 0 model.gnn_type GINEConv 
+    python -m train.graph_property task $task num_workers 8 model.mini_layers 1 model.hops_dim 0 model.gnn_type GINEConv         
+done
+
